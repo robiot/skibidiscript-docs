07a79ff6bb3c0e3e8b404b28671b5bfa1f995ed7
MarkDemidovs <markdemidovs@proton.me>
2024-09-28 22:15:44 +0300

    Basic index page information

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,7 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span>skibidiscript</span>,
     project: {
-      link: 'https://github.com/shuding/nextra'
+      link: 'https://github.com/robiot/skibidiscript'
     }
     // ... other theme options
   }
