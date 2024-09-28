f87ba31cd2f39399c164a620c2cea7cf653b3d57
MarkDemidovs <markdemidovs@proton.me>
2024-09-28 22:31:04 +0300

    Doc Structuring

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,12 @@
 export default {
-    logo: <span>skibidiscript</span>,
+    logo: <strong>skibidiscript</strong>,
     project: {
       link: 'https://github.com/robiot/skibidiscript'
+    },
+    footer: {
+      text: "skibidiscript made by robiot | docs by markdemidovs"
+    },
+    chat: {
+      link: 'https://discord.gg/3Pf8Xu5Kjm'
     }
-    // ... other theme options
   }
